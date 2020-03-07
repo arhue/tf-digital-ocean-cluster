372a34bf74280a10556b2e4ff54d52935cb977bc
Varun Priolkar <me@varunpriolkar.com>
2020-03-07 18:50:06 +0530

    work

--- README.md
+++ README.md
@@ -1,19 +1,2 @@
 # README
-
-This cluster contains Terraform config to create a Kubernetes cluster on Digital Ocean and a shell script that gets a kubeconfig for accessing that cluster.
-
-It assumes you have a Digital Ocean token ([here's how to get one](https://www.digitalocean.com/docs/api/create-personal-access-token/)).
-
-## How to run
-
-```
-$ git clone git@github.com:ponderosa-io/tf-digital-ocean-cluster.git
-$ export TF_VAR_do_token=<your_digital_ocean_token>
-$ export TF_VAR_do_cluster_name=<your_cluster_name>
-$ cd tf-digital-ocean-cluster
-$ terraform plan
-$ terraform apply
-$ ./get_config
-$ export KUBECONFIG=$(pwd)/config
-```
-
+This containers Terraform and helm configs for a task I was asked to undertake.
--- digital-ocean-cluster.tf
+++ digital-ocean-cluster.tf
@@ -1,5 +1,11 @@
 variable "do_token" {}
-variable "do_cluster_name" {}
+
+locals {
+  do_kube_ver = "1.16.6-do.0"
+  region = "blr1"
+  size = "s-2vcpu-2gb"
+  cluster_name = "honest"
+}
 
 # Get a Digital Ocean token from your Digital Ocean account
 #   See: https://www.digitalocean.com/docs/api/create-personal-access-token/
@@ -9,17 +15,112 @@ provider "digitalocean" {
 }
 
 resource "digitalocean_kubernetes_cluster" "my_digital_ocean_cluster" {
-  name    = "${var.do_cluster_name}"
-  region  = "nyc1"
-  version = "1.13.1-do.2"
+  name    = local.cluster_name
+  region  = local.region
+  version = local.do_kube_ver
 
   node_pool {
     name       = "worker-pool"
-    size       = "s-2vcpu-2gb"
+    size       = local.size
     node_count = 2
   }
 }
 
 output "cluster-id" {
   value = "${digitalocean_kubernetes_cluster.my_digital_ocean_cluster.id}"
+}
+
+resource "null_resource" "get" {
+  provisioner "local-exec" {
+    command = "curl -X GET -H \"Content-Type: application/json\" -H \"Authorization: Bearer ${var.do_token}\" \"https://api.digitalocean.com/v2/kubernetes/clusters/${digitalocean_kubernetes_cluster.my_digital_ocean_cluster.id}/kubeconfig\" > kubeconfig"
+  }
+
+  triggers = {
+    build_number = "${timestamp()}"
+  }
+}
+
+data "local_file" "tiller" {
+    filename = "tiller.yaml"
+}
+resource "null_resource" "tiller" {
+  provisioner "local-exec" {
+    working_dir = path.module
+    command = <<EOS
+kubectl --kubeconfig kubeconfig apply -f tiller.yaml
+EOS
+  }
+
+  triggers = {
+    tiller_content = data.local_file.tiller.content
+    cluster_id = digitalocean_kubernetes_cluster.my_digital_ocean_cluster.id
+  }
+}
+
+data "local_file" "mandatory" {
+    filename = "mandatory.yaml"
+}
+resource "null_resource" "mandatory" {
+  provisioner "local-exec" {
+    working_dir = path.module
+    command = <<EOS
+kubectl --kubeconfig kubeconfig apply -f mandatory.yaml
+EOS
+  }
+
+  triggers = {
+    tiller_content = data.local_file.mandatory.content
+    cluster_id = digitalocean_kubernetes_cluster.my_digital_ocean_cluster.id
+  }
+}
+
+data "local_file" "cloud-generic" {
+    filename = "cloud-generic.yaml"
+}
+resource "null_resource" "cloud-generic" {
+  provisioner "local-exec" {
+    working_dir = path.module
+    command = <<EOS
+kubectl --kubeconfig kubeconfig apply -f cloud-generic.yaml
+EOS
+  }
+
+  triggers = {
+    tiller_content = data.local_file.cloud-generic.content
+    cluster_id = digitalocean_kubernetes_cluster.my_digital_ocean_cluster.id
+  }
+}
+
+data "local_file" "cert-manager" {
+    filename = "cert-manager.yaml"
+}
+resource "null_resource" "cert-manager" {
+  provisioner "local-exec" {
+    working_dir = path.module
+    command = <<EOS
+kubectl --kubeconfig kubeconfig apply -f cert-manager.yaml
+EOS
+  }
+
+  triggers = {
+    tiller_content = data.local_file.cert-manager.content
+    cluster_id = digitalocean_kubernetes_cluster.my_digital_ocean_cluster.id
+  }
+}
+
+data "local_file" "prod-issuer" {
+    filename = "prod-issuer.yaml"
+}
+resource "null_resource" "prod-issuer" {
+  provisioner "local-exec" {
+    working_dir = path.module
+    command = <<EOS
+kubectl --kubeconfig kubeconfig apply -f prod-issuer.yaml
+EOS
+  }
+
+  triggers = {
+    tiller_content = data.local_file.prod-issuer.content
+    cluster_id = digitalocean_kubernetes_cluster.my_digital_ocean_cluster.id
+  }
 }
